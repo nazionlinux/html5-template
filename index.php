bf47e1b773ecf46ae81eaa277507baceab042850
nazionlinux <amministratore@nazionlinux.com>
2013-05-16 02:20:31 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,80 @@
+<!doctype html>
+<html lang="it">
+	<head>
+		<meta charset="UTF-8" />
+
+		<title>Your Website</title>
+
+		<link rel="icon" type="image/png" href="./favicon.png" />
+		<!-- Internet Explore 5.5, 6 & 7 does NOT support PNG, GIF and JPEG type icons, only ICO files -->
+		<!--[if IE]><link rel="shortcut icon" href="./favicon.ico" /><![endif]-->
+		<link rel="apple-touch-icon" href="./apple-touch-icon.png">
+
+		<link rel="stylesheet" type="text/css" href="css/style.css" />
+
+	</head>
+	<body>
+		<header id="header">
+			<hgroup>
+				<h1>Main Heading</h1>
+				<h2>Secondary Heading</h2>
+			</hgroup>
+		</header>
+
+		<aside id="colonna-dx">
+			<article>
+				<figure>
+					<img src="img/logo/sm-logo.gif" alt="Smashing Magazine" />
+				</figure>
+				<hgroup>
+					<h4>Featured Article</h4>
+					<h5><a href="#">HTML5 in Smashing Magazine!</a></h5>
+				</hgroup>
+				<p>secondary content</p>
+				<footer>
+					<p>Autore: <a href="#">HTML5 in Smashing Magazine!</a></p>
+				</footer>
+			</article>
+		</aside>
+
+		<nav>
+			<ul>
+				<li><a href="" title="">Link</a></li>
+				<li><a href="" title="">Link</a></li>
+				<li><a href="" title="">Link</a></li>
+				<li><a href="" title="">Link</a></li>
+				<li><a href="" title="">Link</a></li>
+			</ul>
+		</nav>
+
+		<section>Content, which might include headings, paragraphs, article elements each with a header and footer if desired and other material such as figure and figcaption or form elements.</section>
+
+		<section>
+			<article>
+				<header>
+					<h3>Article title</h3>
+					<p>Posted on <time datetime="2009-09-04T16:31:24+02:00">September 4th 2009</time> by <a href="#">Writer</a> - <a href="#comments">6 comments</a></p>
+				</header>
+				<p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.</p>
+				<footer>
+					<p>Autore: <a href="#">HTML5 in Smashing Magazine!</a></p>
+				</footer>
+			</article>
+		
+			<article>
+				<header>
+					<h3>Article title</h3>
+					<p>Posted on <time datetime="2009-09-04T16:31:24+02:00">September 4th 2009</time> by <a href="#">Writer</a> - <a href="#comments">6 comments</a></p>
+				</header>
+				<p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.</p>
+				<footer>
+					<p>Autore: <a href="#">HTML5 in Smashing Magazine!</a></p>
+				</footer>
+			</article>
+		</section>
+
+		<footer id="copyleft">
+			<p>Copyright 2009 Your name <a href="#">HTML5 in Smashing Magazine!</a></p>
+		</footer>
+	</body>
+</html>
